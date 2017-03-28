83ba8c68d83711a9bb65fc31271abe8983fb66e4
Snapple49 <oliver-dot-stein@hotmail.com>
2017-03-28 13:14:15 +0200

    Progress on baud clock generator

--- baud_generator.vhd
+++ baud_generator.vhd
@@ -19,6 +19,7 @@ end package baud_generator;
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
 
 entity generator is
 
@@ -27,7 +28,7 @@ entity generator is
 	);
 
 	port (
-		clk_source, freq_sel, rst : in std_logic;
+		clk_source, freq_sel, rst : in std_logic; -- freq_sel chooses between baud (0) and baud16 (1)
 		clk_baud, clk_baud16 : out std_logic
 	);
 	
@@ -35,21 +36,35 @@ end entity;
 
 architecture behavior of generator is
 
-	signal b_rate : std_logic_vector;
-	signal ctr : std_logic_vector (16 downto 0) := "00000000000000000";
+	signal half_rate : std_logic_vector;
+	signal ctr : std_logic_vector (16 downto 0) := "000000000000000000000";
 
 begin
-	b_rate <= conv_std_logic_vector(baudrate, 17);
+	half_rate <= '0' & conv_std_logic_vector(baudrate, 17)(21 downto 1);
 	process (clk_source, rst)
 	begin
 		if rising_edge(rst) then
-			
-		
+			ctr <= "000000000000000000000";
+			clk_baud <= '0';
+			clk_baud16 <= '0';
 		elsif rising_edge(clk_source) then
-		
+			ctr <= ctr + 1;
 		end if;
-		
 	end process;
 	
+	process (ctr)
+	begin
+		case freq_sel is
+			when 0 => 
+				if (ctr = half_rate-1) then
+					clk_baud <= not clk_baud;
+				end if;	
+			when 1 =>
+				if (ctr = half_rate-1) then
+					clk_baud <= not clk_baud;
+				end if;	
+		
+	
+	end process;
 
 end behavior;
