5c35ff0419132b33cee0ab0790fe5363492052d7
Snapple49 <oliver-dot-stein@hotmail.com>
2017-04-03 15:04:44 +0200

    Updated controller, simulated, seems to work

--- version2/controller.vhd
+++ version2/controller.vhd
@@ -9,9 +9,9 @@ entity controller is
 		clk_baud		: in std_logic;
 		clk_baud16	: in std_logic;
 		RX_in	      : in std_logic;
-		clk_rst		: out std_logic;
-		clk_rst16 	: out std_logic;
-		wrt_reg		: out std_logic
+		clk_rst		: out std_logic := '0';
+		clk_rst16 	: out std_logic := '0';
+		wrt_reg		: out std_logic := '0'
 	);
 
 end entity;
@@ -19,7 +19,7 @@ end entity;
 architecture rtl of controller is
 signal reading : std_logic := '0';
 signal counter8a : std_logic_vector(2 downto 0) := "000";
-signal counter8b : std_logic_vector(2 downto 0) := "000";
+signal counter8b : std_logic_vector(3 downto 0) := "0000";
 signal counter16 : std_logic_vector(3 downto 0) := "0000";
 
 begin
@@ -43,16 +43,26 @@ begin
 	process(clk_baud, reading)
 	begin
 		if (reading = '1') then
-			wrt_reg <= '1';	
-			counter8b <= counter8b + 1;
+			if falling_edge(clk_baud) then
+				wrt_reg <= '1';
+				if (counter8b = "1001") then
+					counter8b <= "0000";
+				else
+					counter8b <= counter8b + 1;
+				end if;
+			end if;
 		end if;
  	end process;
 	
-	process(counter8a)
+	process(counter8a, counter8b)
 	begin
 		if(counter8a = "111")then
 			reading <= '1';
 		end if;
+		if(counter8b = "1001")then
+			reading <= '0';
+		end if;
 	end process;
+
 end rtl;
 
--- version2/rs232_project.vhd
+++ version2/rs232_project.vhd
@@ -14,7 +14,7 @@
 
 -- PROGRAM		"Quartus II 64-Bit"
 -- VERSION		"Version 13.0.1 Build 232 06/12/2013 Service Pack 1 SJ Web Edition"
--- CREATED		"Sun Apr 02 16:45:26 2017"
+-- CREATED		"Mon Apr 03 13:37:52 2017"
 
 LIBRARY ieee;
 USE ieee.std_logic_1164.all; 
@@ -25,10 +25,8 @@ ENTITY rs232_project IS
 	PORT
 	(
 		UART_RX :  IN  STD_LOGIC;
-		pin_name2 :  IN  STD_LOGIC;
-		pin_name5 :  IN  STD_LOGIC;
-		pin_name6 :  IN  STD_LOGIC;
-		pin_name1 :  IN  STD_LOGIC;
+		source_clock :  IN  STD_LOGIC;
+		freq_select :  IN  STD_LOGIC;
 		UART_TX :  OUT  STD_LOGIC;
 		sr_out :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0)
 	);
@@ -71,6 +69,7 @@ SIGNAL	SYNTHESIZED_WIRE_0 :  STD_LOGIC;
 SIGNAL	SYNTHESIZED_WIRE_1 :  STD_LOGIC;
 SIGNAL	SYNTHESIZED_WIRE_2 :  STD_LOGIC;
 SIGNAL	SYNTHESIZED_WIRE_3 :  STD_LOGIC;
+SIGNAL	SYNTHESIZED_WIRE_4 :  STD_LOGIC;
 
 
 BEGIN 
@@ -78,40 +77,44 @@ UART_TX <= UART_RX;
 
 
 
-b2v_inst : clock_generator
+b2v_16baudrate_clock : clock_generator
 GENERIC MAP(baudrate => 5208
 			)
-PORT MAP(clk_source => pin_name5,
-		 freq_sel => pin_name6,
+PORT MAP(clk_source => source_clock,
+		 freq_sel => freq_select,
 		 rst => SYNTHESIZED_WIRE_0,
+		 clk_baud => SYNTHESIZED_WIRE_3);
+
+
+b2v_baudrate_clock : clock_generator
+GENERIC MAP(baudrate => 5208
+			)
+PORT MAP(clk_source => source_clock,
+		 freq_sel => SYNTHESIZED_WIRE_1,
+		 rst => SYNTHESIZED_WIRE_2,
 		 clk_baud => clk_baud);
+
+
+SYNTHESIZED_WIRE_1 <= NOT(freq_select);
+
 
 
 b2v_inst3 : controller
 PORT MAP(clk_baud => clk_baud,
-		 clk_baud16 => SYNTHESIZED_WIRE_1,
+		 clk_baud16 => SYNTHESIZED_WIRE_3,
 		 RX_in => UART_RX,
-		 clk_rst => SYNTHESIZED_WIRE_0,
-		 clk_rst16 => SYNTHESIZED_WIRE_3,
-		 wrt_reg => SYNTHESIZED_WIRE_2);
+		 clk_rst => SYNTHESIZED_WIRE_2,
+		 clk_rst16 => SYNTHESIZED_WIRE_0,
+		 wrt_reg => SYNTHESIZED_WIRE_4);
 
 
 b2v_inst4 : s_reg
 GENERIC MAP(NUM_STAGES => 8
 			)
 PORT MAP(clk => clk_baud,
-		 enable => SYNTHESIZED_WIRE_2,
+		 enable => SYNTHESIZED_WIRE_4,
 		 sr_in => UART_RX,
 		 sr_out => sr_out);
-
-
-b2v_inst5 : clock_generator
-GENERIC MAP(baudrate => 5208
-			)
-PORT MAP(clk_source => pin_name1,
-		 freq_sel => pin_name2,
-		 rst => SYNTHESIZED_WIRE_3,
-		 clk_baud => SYNTHESIZED_WIRE_1);
 
 
 END bdf_type;
